62b5e44c2cd47589d4fe411f9486c08f8e828960
rahatlou <rahatlou@gmail.com>
2019-02-04 17:07:47 +0100

    first draft of repo

---
+++ site/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="author" content="Shahram Rahatlou" />
+<meta name="Description" content="Fisica Nucleare e Subnucleare" />
+
+
+<title>Fisica Nucleare e Subnucleare, Shahram Rahatlou</title>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+<!-- google analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-35197438-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-35197438-3');
+</script>
+
+<!-- google analytics -->
+
+
+</head>
+<body>
+<div id="header">
+
+<div id="logo">
+	<h1>Fisica Nucleare e Subnucleare</h1>
+</div>
+<!-- end #logo -->
+
+<div id="cssmenu">
+  <ul>
+    <li><a href="../index.php">Home</a></li>
+    <li class='has-sub'><a href="../didattica.php">Didattica</a>
+       <ul>
+         <li><a href="../cmp/">Computing Methods for Physics</a></li>
+         <li><a href="../particelle/">Particelle Elementari</a></li>
+         <li><a href="../labcalc/">Lab Calcolo</a></li>
+         <li><a href="../programmazione++/">Programmazione++</a></li>
+       </ul>
+    </li>
+    <li><a href="../contact.php">Contact &amp; Calendar</a></li>
+  </ul>
+</div>
+<!-- end #menu -->
+
+</div>
+<!-- end #header -->
+
+<div id="page">
+
+<div id="content">
+  <?php include("frontpage.html") ?>
+</div>
+<!-- end #content -->
+
+<div id="sidebar">
+
+<div id="sidebar-bgtop"></div>
+
+<div id="sidebar-content">
+  <h3 class=h3sidebar>Last update: 4 Feb 2019</h3>
+  Created by
+  <a href="http://www.roma1.infn.it/people/rahatlou/">Shahram Rahatlou</a>
+  [<a href="mailto:shahram.rahatlou@uniroma1.it">shahram.rahatlou (at) uniroma1.it</a>]
+
+
+  <p>
+  <h2>News</h2>
+  <div style="width: 270px; height: 350px; overflow: auto">
+    <ul>  <!-- lista avvisi-->
+      <li>25/9: Le lezioni avranno inizio il giorno mercoledi` 27 Febbraio</li>
+    </ul> <!-- lista avvisi-->
+  </div>
+
+
+  <p>
+  <h2>Reference material</h2>
+  <div style="width: 270px; height: 150px; overflow: auto">
+  <ul>
+    <li>Lecture notes and examples</li>
+    <li>libro 1</li>
+  </ul>
+  </div>
+
+  <h2 class="title">Orario Ricevimento</h2>
+  <ul>
+  <li>Giovedi` 13-15, <a href="http://www.phys.uniroma1.it/mappe/mappe.php?edificio=marconi&piano=2">Stanza 202-A, Piano 2, Edificio Marconi (Vecchio edificio di Fisica)</a>
+    </li>
+  </ul>
+
+
+</div>
+
+<div id="sidebar-bgbtm"></div>
+
+</div>
+<!-- end #sidebar -->
+
+</div>
+<!-- end #page -->
+
+<div id="footer">
+<p>&copy; 2008. All Rights Reserved. Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>.</p>
+</div>
+<!-- end #footer -->
+
+</body>
+</html>
