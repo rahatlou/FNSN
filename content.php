f9da5ec12760db0dbdd4b15088e0817f53be4752
rahatlou <rahatlou@gmail.com>
2019-10-23 17:19:12 +0200

    moved esami ed esoneri to dedicated subpage

--- content.php
+++ content.php
@@ -1,9 +1,9 @@
 <div id="content">
-	<h1>Fisica Nucleare e Subnucleare I</h1>
+	<h1>Fisica Nucleare e Subnucleare (2018/19)</h1>
 
 	<div class="post">
 
-	<h2 class="title">Orario Lezioni</h2>
+	<h3 class="title">Orario Lezioni</h3>
 	<div class="entry">
 		<p><b>Il corso dell'anno accademico 2018/19 inizia il giorno mercoledi` 27 Febbraio 2019</b>
 			<ul>
@@ -24,141 +24,7 @@
 	per l'accesso al servizio e il riempimento dei moduli.
 	</div> <!--- end of entry -->
 
-	<h2 class="title">Esoneri</h2>
 
-	<div class="entry">
-	  <p>
-	    Sara` possibile consultare dispense, appunti, e formulari durante le prove
-	    di esonero ma
-	    <b>non si possono portare o/e consultare esercizi svolti</b>.
-	    </p>
-
-	    <p><a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQZYHuGJJVvoO5BOHtwY8tqu7aKRi9WG0D18V-AW5Ecnvh3xrzN_-I5jJpX8bySplBbnkbi5ptrDfzF/pubhtml?gid=1187315539&single=true">Risultati delle prove di esonero e bonus</a><br>
-	      Voti validi per tutte le sessioni di esame fino a Febbraio 2020. Non validi dopo rifiuto voto orale.</p>
-
-	  <ul>
-	    <li> Primo bonus 2019-04-17: <a href="Didattica/FNSN/Esercizi/20190417_bonus_sol.pdf">testo e soluzioni</a></li>
-	    <li> Primo esonero: venerdi` 3 maggio 2019 <b>ore 16:00</b>:
-	      <!-- <a href="https://docs.google.com/forms/d/e/1FAIpQLSdRJq-BLndhFqBupZokMgdZL54Z0Tpjwnb28DGmdhG0Pq1N3Q/viewform?usp=sf_link">Modulo prenotazione entro ore 14:00 di Giovedi` 2 maggio</a>. E` richiesta l'autenticazione tramite il vostro indirizzo email Sapienza.
-	      Potrebbe essere necessario, prima sconnettere il vostro account gmail privato nel browser. -->
-	        Per gli studenti di questo canale la prova avra` inizio alle <b>ore 16:00</b>
-	        in <b>Aula Amaldi</b>. La durata della prova sara` di 2 ore.
-	         (<a href="Didattica/FNSN/Esercizi/20190503-esonero.pdf">testo e soluzioni</a>)
-	         (<a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQZYHuGJJVvoO5BOHtwY8tqu7aKRi9WG0D18V-AW5Ecnvh3xrzN_-I5jJpX8bySplBbnkbi5ptrDfzF/pubhtml?gid=2132548607&single=true">Risultati</a>)
-
-	    </li>
-	    <li> Secondo bonus 2019-05-31: consegna dell soluzione individuale e cartacea il giorno 7/6 a lezione oppure all'esonero.
-	      (<a href="Didattica/FNSN/Esercizi/20190531_bonus-v2.pdf">pdf</a>) (<a href="Didattica/FNSN/Esercizi/20190531_bonus_sol.pdf">pdf con soluzioni</a>)<br>
-	       <b>Errata corrige (03/06)</b>: il testo dell’esercizio 1 è stato aggiornato aggiungendo la distanza fra una stazione spettrometrica
-	       e la successiva (1 metro). (<a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQZYHuGJJVvoO5BOHtwY8tqu7aKRi9WG0D18V-AW5Ecnvh3xrzN_-I5jJpX8bySplBbnkbi5ptrDfzF/pubhtml?gid=1233151116&single=true">risultati </a>)
-	    </li>
-	    <li> Secondo esonero: venerdi` 7 giugno 2019, inizio ore 15:30 in Aula Amaldi (solo per questo canale) (<a href="Didattica/FNSN/Esercizi/20190607-esonero.pdf">pdf con soluzioni</a>)
-	    (<a href="https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vQZYHuGJJVvoO5BOHtwY8tqu7aKRi9WG0D18V-AW5Ecnvh3xrzN_-I5jJpX8bySplBbnkbi5ptrDfzF/pubhtml?gid=929776904&single=true">risultati</a>)</li>
-	  </ul>
-	</div>
-
-	<h2 class="title">Esami</h2>
-	<div class="entry">
-	<p>Tutte le date sono state inserite su <a href="https://www.uniroma1.it/it/pagina-strutturale/studenti">infostud</a>.
-	</p>
-
-	<p>
-	E` disponibile una <a href="http://www.roma1.infn.it/~ippolitv/fns1_compiti/">raccolta di esercizi d'esame</a>
-	degli anni passati.</p>
-
-
-	<p> <a href="https://docs.google.com/document/d/1H-NFc5shVc6bVdUwWP-WaGsehikJHadOtNB4scLzmoE/edit?usp=sharing">Regolamento per l'esame</a>
-	del corso di Fisica Nucleare e Subnucleare, A.A. 2018-19</p>
-
-	  <ul>
-	     <li><b>Appello 6 Settembre 2019</b>: La prova si svolgera` in
-	    <b>Aula 3</b> alle ore <b>9:00</b>.
-
-	    <p>I risultati saranno disponibili entro lunedi` 9 in serata.  (<a
-	    href="Didattica/FNSN/Esercizi/20190906-esame_sol.pdf">testo e soluzioni</a>)(<a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQZYHuGJJVvoO5BOHtwY8tqu7aKRi9WG0D18V-AW5Ecnvh3xrzN_-I5jJpX8bySplBbnkbi5ptrDfzF/pubhtml?gid=1315018563&single=true">risultati</a>)</p>
-
-	    <p>
-	     Le prove orali si svolgeranno nei giorni 10/9 ore 9-15, 12/9 ore
-	     10-16, 16/9 ore 9-14. Sara` come sempre possibile indicare la
-	     vostra preferenza nel modulo che sara` disponibile prima di
-	    martedi`. (<a
-	     href="https://docs.google.com/forms/d/e/1FAIpQLSeL40aMM-knUfgev7FlCjMOW26mZSmnaGfIqV5S2sf39EPNjA/viewform?usp=sf_link">prenotazione
-	     prova orale</a>)
-	    </p>
-
-	     </li> <!-- Fine 6 Sep -->
-
-
-
-	    <li><b>Appello 5 Luglio 2019</b>: La prova si svolgera` in <b>Aula Amaldi</b> alle ore <b>9:00</b>.
-	      (<a href="Didattica/FNSN/Esercizi/20190705-esame_sol.pdf">testo con le soluzioni</a>)
-
-	      <p>
-	        E` disponibile il
-	        <a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ94HzV6vYZeFGL_qRLGeeqPRzCSQ9pMLnrTqjipPjvRykeM_Vh-q0vG6mQwjLbF-NN6iXhk0HbYBhX/pubhtml?gid=1364362029&single=true">calendario delle prove orali</a>.
-	      </p>
-
-	      <p>I risultati della prova scritta verrano pubblicati entro martedi` (9/7) sera.
-	        (<a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQZYHuGJJVvoO5BOHtwY8tqu7aKRi9WG0D18V-AW5Ecnvh3xrzN_-I5jJpX8bySplBbnkbi5ptrDfzF/pubhtml?gid=685896648&single=true">Risultati</a>)</p>
-
-	      <p>
-	      Gli studenti sono invitati ad <a href="https://docs.google.com/forms/d/e/1FAIpQLSdwdvGoewwCKnvfPA1_RQdM3iWlu_6eml_Kfy3hB8KYDm6jFg/viewform?usp=sf_link">indicare la la preferenza per la prova orale</a>.
-	      </p>
-
-	      <p>Gli studenti che intendono sostenere l'esame sono pregati di
-	       <a href="https://docs.google.com/forms/d/e/1FAIpQLSdpGyeVdfZU7Dagt7ZXuu-oxMMpHPuorUO2l7V8TJrE7QZ1fA/viewform?usp=sf_link">riempire questo questionario</a>
-	       entro le <b>ore 12 di lunedi` 1 luglio</b>, per poter organizzare la sessione
-	       dato il numero limitato di posti per la prova scritta.</p>
-	     </li> <!-- 5 Luglio -->
-
-	     <li><b>Appello 17 giugno 2019</b>: Gli studenti che intendono sostenere l'esame sono pregati di
-	     <a href="https://docs.google.com/forms/d/e/1FAIpQLSfYyL0Muk6MIGyL0m9gyx9j3s8Ch8ArwX61Jt6lkDrDgxL7zA/viewform?usp=sf_link">riempire questo questionario</a>
-	     entro le <b>ore 12 di giovedi` 13 giugno</b>, per poter organizzare la sessione
-	       dato il numero limitato di posti per la prova scritta.
-	       <br>
-	       La prova scritta per gli studenti di questo canale avra` luogo in <b>Aula Amaldi</b>
-	       lunedi` <b>17/6</b> alle <b>ore 9</b>.
-	       <br>
-	       <b>Sara` possibile sostenere la prova scritta solo se fatta la prenotazione  su infostud per l'appello.</b>
-	       <p>
-	         I risultati della prova scritta e del recupero degli esoneri saranno pubblicati su questo sito
-	         entro mercoledi` (19/6) sera.
-	         (<a href="Didattica/FNSN/Esercizi/20190617-esame.pdf">testo prova scritta</a>)(<a href="Didattica/FNSN/Esercizi/20190617-esame_sol.pdf">soluzioni</a>)
-	       </p>
-
-	       <p><a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vQZYHuGJJVvoO5BOHtwY8tqu7aKRi9WG0D18V-AW5Ecnvh3xrzN_-I5jJpX8bySplBbnkbi5ptrDfzF/pubhtml?gid=880586222&single=true">Risultati della prova scritta</a><br>
-	       Sara` possibile visionare il compito a partire da lunedi` 24 giugno ore 9:30 durante le prove orali.
-	       </p>
-
-
-	       <p>
-	         Le prove orali cominceranno a partire da lunedi` 24 giugno nell'ufficio del docente (Edficio Marconi, Stanza 202-A).
-	         (<a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vTVnmcirSADVOwGFc0TFZoQAQbqxC_9ViWdG2UWf8xtlX5OElR0pfnzoZyv7E_0uDx3Gv6QsVQ_APIW/pubhtml?gid=324161520&single=true">calendario prove orali</a>)
-
-	         <p>
-	         Gli studenti sono invitati ad <a href="https://docs.google.com/forms/d/e/1FAIpQLSep55R8F28TVkFB905ODfpriP8hNXOmQVzMlHYuHjSnzhcQQg/viewform?usp=sf_link">indicare la la preferenza per la data e il giorno</a>.
-	       </p>
-	         <ol>
-	           <li> il numero di posti e` limitato per ciascuna sessione e sono ad esaurimento</li>
-	           <li> e` possibile indicare una sola data</li>
-	           <li> selezionare una data solo se certi di voler sostenere l'esame</li>
-	           <li>comunicare al docente le cancellazioni per permettere ad altri a sostenere l'esame </li>
-	           <li> se non si trova alcuna sessione libera contattare il docente via email</li>
-	           <li>La precedenza va a coloro che si sono regolarmente prenotati su infostud. Chi non si e` prenotato
-	              potra` sostenere la prova <b>solo</b> se ci saranno posti disponibili. Altrimenti dovranno prenotarsi in tempo
-	              per il secondo appello.</li>
-	           <li>Per modifiche o variazioni contattare il docente via email.</li>
-	        </ol>
-
-
-	       </li> <!-- fine appello 17 giugno -->
-	     <li>Appello straordinario del 27 maggio:
-	A causa del blocco delle attivita` didattiche il giorno 27 maggio,
-	la prova scritta dell'appello straordinario avra` luogo il giorno
-	30 maggio alle ore 14 in Aula 6.</li>
-	  </ul>
-
-	</div>
 
 	<h2 class="title">Lezioni</h2>
 	<div class="entry">
