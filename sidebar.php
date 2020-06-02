5496e9ed5ad336a5d96aed71880c82913d653d28
rahatlou <rahatlou@gmail.com>
2020-06-02 11:45:40 +0200

    php version of sidebar and standalone news

---
+++ sidebar.php
@@ -0,0 +1,38 @@
+<div id="sidebar-content">
+
+<h3 class=h3sidebar>Last update: 27 May 2020</h3>
+  Created by
+  <a href="http://www.roma1.infn.it/people/rahatlou/">Shahram Rahatlou</a>
+  [<a href="mailto:shahram.rahatlou@uniroma1.it">shahram.rahatlou (at) uniroma1.it</a>]
+
+<h2><a href="https://calendar.google.com/calendar/b/1?cid=dW5pcm9tYTEuaXRfY2xhc3Nyb29tODY2ZjBmNjlAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ">Calendario videolezioni e ricevimento</a></h2>
+<h2><a href="https://meet.google.com/ozv-eooz-rde?authuser=1&hs=122">Google Meet: ozv-eooz-rde</a></h2>
+<h2><a href="index.php?link=Didattica&sublink=FNSN/esami">Esami ed Esoneri</a></h2>
+<h2><a href="index.php?link=Didattica&sublink=2019.FNSN">Sito AA 2018/19</a></h2>
+
+
+<div class="post news">
+  <?php include("Didattica/FNSN/news.html") ?>
+</div>
+
+  <h2>Libri e referenze utili</h2>
+  <div style="height: 350px; overflow: auto">
+  <ul>
+    <li>R. Paramatti, <a href="http://www.roma1.infn.it/cms/ric/cinematica.pdf">Dispense di cinematica relativistica</a></li>
+    <li>D. Griffiths, <a href="https://www.wiley.com/en-us/Introduction+to+Elementary+Particles+%2C+2nd%2C+Revised+Edition-p-9783527406012">Introduction to Elementary Particles , 2nd Ed.</a></li>
+    <li>D. H. Perkins, <a href="https://doi.org/10.1017/CBO9780511809040">Introduction to High Energy Physics, 4th ed.</a></li>
+    <li>A. Das and T. Ferbel, <a href="https://www.worldscientific.com/worldscibooks/10.1142/5460">Introduction to Nuclear and Particle Physics, 2nd ed.</a></li>
+    <li>Cahn and Goldhaber, <a href="https://doi.org/10.1017/CBO9780511609923">The experimental foundation of Particle Physics</a>, 2nd Ed.</li>
+    <li>C. Bertulani, <a href="https://press.princeton.edu/titles/8455.html">Nuclear Physics in a Nutshell</a></li>
+    <li>C. Dionisi e E. Longo, <a href="http://www.roma1.infn.it/people/longo/fnsn/testo.html">Dispense di fisica nucleare e subnucleare</a></li>
+    <li><a href="http://www.roma1.infn.it/~ippolitv/fns1_compiti/" target="_blank">Esercizi degli anni precedenti</a></li>
+  </ul>
+  </div>
+
+  <h2>Orario Ricevimento</h2>
+  <ul>
+    <li>Docente: Mercoledi` 14-16, <a href="http://www.phys.uniroma1.it/mappe/mappe.php?edificio=marconi&piano=2">Stanza 202-A, Piano 2, Edificio Marconi </a></li>
+    <li>Francesco Pandolfi: Lunedi` 14-16, <a href="http://www.phys.uniroma1.it/mappe/mappe.php?edificio=marconi&piano=2">Stanza 245-b, Piano 2, Edificio Marconi</a> </li>
+  </ul>
+
+</div> <!-- sidebar -->
